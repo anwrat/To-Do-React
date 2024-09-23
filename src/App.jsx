e0c8d6d73dc8ad18b128315f982786814f35654d
anwrat <anweshrwt22@gmail.com>
2024-09-24 05:03:24 +0545

    Making App.jsx and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import './styles.css'
 
 export default function App(){
-  return "Hello there"
+  return (
+  <>{/*This is a fragment*/}
+    <form className='new-item-form'>
+      <div className='form-row'>
+        <label className='toplabel' htmlFor='item'>Item</label>
+        <input type="text" id="item"></input>
+      </div>
+      <button className='btn'>Add</button>
+      <header className='header'>Item List</header>
+    </form>
+  </>
+  )
 }
